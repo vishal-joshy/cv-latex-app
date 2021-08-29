9e6587d37e590e32560e5a5d430cc1ef6513581b
VishalJoshy <1812-vishal@protonmail.com>
2021-08-29 17:31:40 +0530

    Collect general data and store in formcompoenent

--- src/components/FormComponent/FormComponent.js
+++ src/components/FormComponent/FormComponent.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import EducationForm from './EducationForm/EducationForm';
 import ExperienceForm from './ExperienceForm/ExperienceForm';
 import GeneralForm from './GeneralForm/GeneralForm';
 
 function FormComponent() {
+	const [data, setData] = useState({ generalData: '', educationData: '', experienceData: '' });
+	const getGeneralData = (inputData) => {
+		setData({
+			generalData: inputData,
+		});
+		console.log(data);
+	};
+	const getEducationData = (inputData) => {
+		setData({
+			educationData: inputData,
+		});
+	};
+	const getExperienceData = (inputData) => {
+		setData({
+			experienceData: inputData,
+		});
+		console.log(data);
+	};
+
 	return (
 		<div>
 			<form action="onSubmitForm"></form>
-			<GeneralForm />
-			<EducationForm />
-			<ExperienceForm />
+			<GeneralForm getGeneralData={getGeneralData} />
+			<EducationForm getEducationData={getEducationData} />
+			<ExperienceForm getExperienceData={getExperienceData} />
 			<button type="submit">Submit</button>
 		</div>
 	);
--- src/components/FormComponent/GeneralForm/GeneralForm.js
+++ src/components/FormComponent/GeneralForm/GeneralForm.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function GeneralForm() {
+function GeneralForm({ getGeneralData }) {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [phone, setPhone] = useState('');
+
+	const handleName = (e) => {
+		setName(e.target.value);
+		getGeneralData({ name: name, email: email, phone: phone });
+	};
+	const handleEmail = (e) => {
+		setEmail(e.target.value);
+		getGeneralData({ name: name, email: email, phone: phone });
+	};
+	const handlePhone = (e) => {
+		setPhone(e.target.value);
+		getGeneralData({ name: name, email: email, phone: phone });
+	};
+
 	return (
 		<div>
-            <h1>GeneralForm</h1>
-			<input type="text" placeholder="Name"></input>
-			<input type="text" placeholder="Email"></input>
-			<input type="text" placeholder="Phone"></input>
+			<h1>GeneralForm</h1>
+			<input type="text" placeholder="Name" onChange={handleName}></input>
+			<input type="text" placeholder="Email" onChange={handleEmail}></input>
+			<input type="text" placeholder="Phone" onChange={handlePhone}></input>
 		</div>
 	);
 }
